649f8b53c125277effc062045414c90a7e626a4c
Malcolm Mathew <malcolmmmathew@gmail.com>
2017-01-19 17:40:23 -0500

    Added loops for report creation.

--- main.cpp
+++ main.cpp
@@ -17,17 +17,19 @@ std::uniform_real_distribution<double> distribution(0.0,1.0);
 std::queue<packet> router_queue;
 
 // Parameters that we need. This needs to become cmd line arguments later.__a
-int TICKS = 100000;
+int TICKS = 100000*1000;
 int queue_size = -1;
 
 // According to p = lambda* L / C
-double lambda = 200;
-int TICK_SIZE = 10000;
+double lambda = 0;
+int TICK_SIZE = 1 / 0.000001;
 // wut is dis
-int packet_size = 2000;
+int L_packet_size = 2000;
 //rate in seconds
-int transmission_rate = 1000000 / TICK_SIZE;
+int C_transmission_rate = 1000000; 
 int idle_counter = 0;
+
+double num_of_packets_in_queue_total = 0;
 
 int calculate_arrival_time() {
 
@@ -35,6 +37,8 @@ int calculate_arrival_time() {
     
     // Arrival time in seconds
     double arrival_time = (-1 / lambda) * log (1 - U);
+
+    // std::cout << "Arrival Time: " << arrival_time << std::endl;
 
     // Currently assuming 1 tick = 100 millisecond (100 * 10^-3)
     return arrival_time * TICK_SIZE;
@@ -46,9 +50,9 @@ void arrival(int current_time) {
         arrival_time = current_time + calculate_arrival_time();
 
         if (queue_size == -1 || (queue_size > 0 && router_queue.size() < queue_size)) {
-            std::cout << "Added new packet at time " << current_time << std::endl;
+            //std::cout << "Added new packet at time " << current_time << std::endl;
             packet new_packet;
-            new_packet.ticks_until_departure = (packet_size / transmission_rate);
+            new_packet.ticks_until_departure = (L_packet_size / C_transmission_rate / TICK_SIZE);
             router_queue.push(new_packet);
         }
     }
@@ -56,10 +60,10 @@ void arrival(int current_time) {
 }
 
 void server(int current_time) {
-    if (router_queue.size() > 0) {ticks_until_departure
+    if (router_queue.size() > 0) {
         // TODO: Does the first tick count, if so, this should check if == 1.
         if (router_queue.front().ticks_until_departure == 1) {
-            std::cout << "Removed packet at time " << current_time << std::endl;
+            //std::cout << "Removed packet at time " << current_time << std::endl;
             router_queue.pop();
         } else {
             router_queue.front().ticks_until_departure -= 1;
@@ -70,10 +74,8 @@ void server(int current_time) {
 }
 
 
-int main()
+int yolo()
 {
-  std::cout << "Hello!";
-
   // To create a new random uniform number:
   // double dirty_uniform_number =  distribution(generator);
 
@@ -82,14 +84,31 @@ int main()
 
   for (int i=0; i < TICKS; i++) {
 
-      if (i % 1000 == 0) {
-          std::cout << "Progress: " << i << "out of" << TICKS << "%" << std::endl;
-      }
-
       arrival(i);
       server(i);
-  }
 
-    std::cout << idle_counter  << std::endl;
+      num_of_packets_in_queue_total += router_queue.size();
+  }
+}
+
+int main() {
+    for(double r0 = 0.2; r0 <= 0.9; r0+=0.1) {
+        queue_size = -1;
+        TICKS = 100000*1000;
+        TICK_SIZE = 1 / 0.000001;
+        L_packet_size = 2000;
+        C_transmission_rate = 1000000;
+        idle_counter = 0;
+        num_of_packets_in_queue_total = 0;
+        lambda = r0 * C_transmission_rate / L_packet_size;
+
+        yolo();
+
+        std::cout << "Idle counter: " << idle_counter  << std::endl;
+        std::cout << "E[N] " << num_of_packets_in_queue_total / TICKS << std::endl;
+
+
+
+    }
 
 }
